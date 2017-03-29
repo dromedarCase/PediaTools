8dce7179a69583c2e19a142c64d9236aa98487dd
David Malaschonok <davidmalaschonok@gmail.com>
2017-03-29 20:44:19 +0200

    made wikiGrep script-friendly

--- wikiGrep.py
+++ wikiGrep.py
@@ -6,8 +6,9 @@ import urllib.request
 string = sys.argv[1]
 language = sys.argv[2]
 
-print ("Looking for string:", string)
-print ("Wikipedia language:", language)
+print ("Looking for string:", string, file=sys.stderr)
+print ("Wikipedia language:", language, file=sys.stderr)
+print (file=sys.stderr)
 
 for line in sys.stdin:
     url = "http://" + language + ".wikipedia.org" + line
